0853b2a1dd3b62bd1ffa1d8243cd68f09c4c78a1
ivanzuglic <63290877+ivanzuglic@users.noreply.github.com>
2020-04-08 21:25:56 +0200

    Transmitter 1st version

--- UartController/transmitter.vhd
+++ UartController/transmitter.vhd
@@ -29,13 +29,68 @@ use IEEE.STD_LOGIC_1164.ALL;
 --library UNISIM;
 --use UNISIM.VComponents.all;
 
-entity transmitter is
+entity transmitter is PORT (
+	clk: 		in std_logic;
+	rst: 		in std_logic;
+	tick: 	in std_logic;
+	w_data: 	in std_logic_vector(7 downto 0);
+	w_start: in std_logic;
+	tx: 		out std_logic;
+	w_done: 	out std_logic
+	);
 end transmitter;
 
 architecture Behavioral of transmitter is
+	type state is (idle, start, transmit);
+	signal curr_state, next_state : std_logic;
+	signal cnt : integer range 0 to 15 := 0;
+	signal b_i : integer range 0 to 7 := 0;
+	--w_done := 1; idle // kako staviti pocetne vrijednosti
+	--tx := 1; idle
 
 begin
-
+	process(w_start, b_i, state, tick) --tick umjesto cnt?, mozda w_start ne treba
+	begin
+		case state is
+			when idle =>
+				if ((w_start = 1) and (w_done = 1)) then -- dogovor
+					next_state <= start;
+					w_done <= 0;
+					cnt <= 0;
+				end if;
+			when start =>
+				if cnt < 7 then
+					next_state <= start;
+					cnt <= cnt +1;
+				elsif cnt = 7 then
+					tx <= 0;				-- start bit
+					next_state <= transmit;
+					cnt <= 0;
+				end if;			
+			when transmit =>
+				if cnt < 15 then
+					cnt <= cnt +1;
+				elsif cnt = 15 then
+					tx <= w_data(b_i);
+					b_i <= b_i + 1;
+					cnt <= 0;
+				elsif ((cnt = 15) and (b_i = 7)) then -- 8?
+					tx <= 1; 			-- stop bit
+					w_done <= 1;
+					next_state <= idle;
+				end if;
+			when others =>
+				null;
+		end case;
+	end process;
+	
+	process(clk, rst)
+	begin
+		if rising_edge(clk) then
+			curr_state <= next_state;
+		elsif reset = '1' then
+			curr_state <= idle;
+		end if;
+	end process;
 
 end Behavioral;
-
